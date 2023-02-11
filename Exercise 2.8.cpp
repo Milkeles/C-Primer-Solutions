924d177e71c3e37d19023ce3184308fbde4099b5
Hristo Hristov <91317021+Milkeles@users.noreply.github.com>
2023-02-11 20:59:06 +0200

    Update Exercise 2.8.cpp

--- Exercise 2.8.cpp
+++ Exercise 2.8.cpp
@@ -6,3 +6,5 @@ int main()
     cout << "\062\115\n"; // \062 = 2, \115 = M, \n = new line.
     cout << "\062\t\115\n"; // \t = horizontal tab.
 }
+
+// Note: Use ASCII table for reference (https://www.asciitable.com/?utm_content=cmp-true)
